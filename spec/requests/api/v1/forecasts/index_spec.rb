f7a676fff39b44c1c14cd14e610c71fa22034f49
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-03-05 12:37:46 -0700

    Create test for Forecast Index endpoint

---
+++ spec/requests/api/v1/forecasts/index_spec.rb
@@ -0,0 +1,105 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast Endpoint' do
+  context 'happy path' do
+    it 'sends forecast data of a given location' do
+      VCR.use_cassette('denver-forecast') do
+        location = "Denver,CO"
+
+        get "/api/v1/forecast?location=#{location}"
+
+        results = JSON.parse(response.body, symbolize_names: true)
+
+        expect(results).to have_http_status(200)
+        expect(results).to have_key(:data)
+        expect(results[:data]).to be_a(Hash)
+
+        data = results[:data]
+        expect(data).to have_key(:id)
+        expect(data[:id]).to eq("null")
+
+        expect(data).to have_key(:type)
+        expect(data[:type]).to eq("forecast")
+
+        expect(data).to have_key(:attributes)
+        expect(data[:attribues]).to be_a(Hash)
+
+        attributes = data[:attributes]
+        expect(attributes).to have_key(:current_weather)
+        expect(attributes[:current_weather]).to be_a(Hash)
+
+        expect(attributes).to have_key(:daily_weather)
+        expect(attributes[:daily_weather]).to be_a(Hash)
+
+        expect(attributes).to have_key(:hourly_weather)
+        expect(attributes[:hourly_weather]).to be_a(Hash)
+
+        current_weather = attributes[:current_weather]
+        expect(current_weather).to have_key(:datetime)
+        expect(current_weather[:datetime]).to be_a(String)
+
+        expect(current_weather).to have_key(:sunrise)
+        expect(current_weather[:sunrise]).to be_a(String)
+
+        expect(current_weather).to have_key(:sunset)
+        expect(current_weather[:sunset]).to be_a(String)
+
+        expect(current_weather).to have_key(:temperature)
+        expect(current_weather[:temperature]).to be_a(Float)
+
+        expect(current_weather).to have_key(:feels_like)
+        expect(current_weather[:feels_like]).to be_a(Float)
+
+        expect(current_weather).to have_key(:humidity)
+        expect(current_weather[:humidity]).to be_a(Integer)
+
+        expect(current_weather).to have_key(:uvi)
+        expect(current_weather[:uvi]).to be_a(Float)
+
+        expect(current_weather).to have_key(:visibility)
+        expect(current_weather[:visibility]).to be_a(Integer)
+
+        expect(current_weather).to have_key(:conditions)
+        expect(current_weather[:conditions]).to be_a(String)
+
+        expect(current_weather).to have_key(:icon)
+        expect(current_weather[:icon]).to be_a(String)
+
+        daily_weather = attributes[:daily_weather]
+        expect(daily_weather).to have_key(:date)
+        expect(daily_weather[:date]).to be_a(String)
+
+        expect(daily_weather).to have_key(:sunrise)
+        expect(daily_weather[:sunrise]).to be_a(String)
+
+        expect(daily_weather).to have_key(:sunset)
+        expect(daily_weather[:sunset]).to be_a(String)
+
+        expect(daily_weather).to have_key(:max_temp)
+        expect(daily_weather[:max_temp]).to be_a(Float)
+
+        expect(daily_weather).to have_key(:min_temp)
+        expect(daily_weather[:min_temp]).to be_a(Float)
+
+        expect(daily_weather).to have_key(:conditions)
+        expect(daily_weather[:conditions]).to be_a(String)
+
+        expect(daily_weather).to have_key(:icon)
+        expect(daily_weather[:icon]).to be_a(String)
+
+        hourly_weather = attributes[:hourly_weather]
+        expect(hourly_weather).to have_key(:time)
+        expect(hourly_weather[:time]).to be_a(String)
+
+        expect(hourly_weather).to have_key(:temperature)
+        expect(hourly_weather[:temperature]).to be_a(Float)
+
+        expect(hourly_weather).to have_key(:conditions)
+        expect(hourly_weather[:conditions]).to be_a(String)
+
+        expect(hourly_weather).to have_key(:icon)
+        expect(hourly_weather[:icon]).to be_a(String)
+      end 
+    end
+  end
+end
