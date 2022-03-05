3f292c120023a8d2c14abc4ba8e5088ccc5cae02
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-03-05 13:29:43 -0700

    Feat: LocationService#get_lat_long passing tests

---
+++ app/services/location_service.rb
@@ -0,0 +1,18 @@
+class LocationService
+
+  def get_lat_long(location)
+    response = conn.get("geocoding/v1/address?location=#{location}")
+    body = parse_json(response)
+  end
+
+  private
+  def conn
+    Faraday.new(url: "http://www.mapquestapi.com") do |faraday|
+      faraday.params['key'] = ENV['mapquest_id']
+    end
+  end
+
+  def parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
---
+++ spec/services/location/location_service_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe LocationService do
+  context 'class methods' do
+    context '#get_lat_long' do
+      it 'returns location data' do
+        VCR.use_cassette('denver-lat-long') do
+          location = "Denver,CO"
+          query = LocationService.new
+          result = query.get_lat_long(location)
+
+          expect(result).to be_a(Hash)
+          expect(result).to have_key(:results)
+          expect(result[:results]).to be_a(Array)
+          expect(result[:results].first).to be_a(Hash)
+
+          results = result[:results].first
+
+          expect(results). to have_key(:locations)
+          expect(results[:locations]).to be_a(Array)
+          expect(results[:locations].first).to be_a(Hash)
+
+          location = results[:locations].first
+
+          expect(location).to have_key(:latLng)
+          expect(location[:latLng]).to be_a(Hash)
+
+          lat_long = location[:latLng]
+
+          expect(lat_long).to have_key(:lat)
+          expect(lat_long[:lat]).to be_a(Float)
+
+          expect(lat_long).to have_key(:lng)
+          expect(lat_long[:lng]).to be_a(Float)
+        end
+      end
+    end
+  end
+end
