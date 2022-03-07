68c1c278ef5424037d49874d22ce2b5dd3bda950
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-03-07 11:24:41 -0700

    Add styling to current weather temp to fit requirements

--- app/serializers/books_serializer.rb
+++ app/serializers/books_serializer.rb
@@ -2,24 +2,23 @@ class BooksSerializer
   #include JSONAPI::Serializer
   class << self
     def serialize(data, location)
-      require "pry"; binding.pry
-      { data: {
-              id: nil,
-              type: 'books',
-              attributes: {
+      { data:
+        { id: nil,
+          type: 'books',
+          attributes: {
                   destination: location,
                   forecast: forecast(data),
                   total_books_found: data[:count],
                   books: data[:books]
-                          }
-              }
+                      }
+        }
       }
     end
 
     def forecast(data)
       {
         summary: data[:weather].current_weather.conditions,
-        temperature: data[:weather].current_weather.temperature
+        temperature: data[:weather].current_weather.temperature.to_s + " F"
       }
     end
   end
