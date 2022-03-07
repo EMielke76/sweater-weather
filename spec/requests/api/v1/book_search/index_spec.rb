ee1ec152718129cdb774d94de4df8982bc34717b
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-03-07 11:42:33 -0700

    Create test for empty quantity param

--- spec/requests/api/v1/book_search/index_spec.rb
+++ spec/requests/api/v1/book_search/index_spec.rb
@@ -87,5 +87,69 @@ RSpec.describe "BookSearch endpoint" do
         expect(results[:message]).to eq("No location specified")
       end
     end
+
+    it 'defaults to 5 if no quantity given' do
+      VCR.use_cassette('missing-quantity') do
+        location = "denver,co"
+
+        get "/api/v1/book-search?location=#{location}&quantity="
+
+        results = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(200)
+
+        expect(results).to be_a(Hash)
+        expect(results).to have_key(:data)
+        expect(results[:data]).to be_a(Hash)
+
+        data = results[:data]
+
+        expect(data).to have_key(:id)
+        expect(data[:id]).to eq(nil)
+
+        expect(data).to have_key(:type)
+        expect(data[:type]).to eq("books")
+
+        expect(data).to have_key(:attributes)
+        expect(data[:attributes]).to be_a(Hash)
+
+        attributes = data[:attributes]
+        expect(attributes).to have_key(:destination)
+        expect(attributes[:destination]).to eq("#{location}")
+
+        expect(attributes).to have_key(:forecast)
+        expect(attributes[:forecast]).to be_a(Hash)
+
+        expect(attributes).to have_key(:total_books_found)
+        expect(attributes[:total_books_found]).to be_a(Integer)
+
+        expect(attributes).to have_key(:books)
+        expect(attributes[:books]).to be_a(Array)
+        expect(attributes[:books].count).to eq(5)
+
+        forecast = attributes[:forecast]
+        expect(forecast).to have_key(:summary)
+        expect(forecast[:summary]).to be_a(String)
+
+        expect(forecast).to have_key(:temperature)
+        expect(forecast[:temperature]).to be_a(String)
+
+        book = attributes[:books].first
+        expect(book).to have_key(:isbn)
+        expect(book[:isbn]).to be_a(Array)
+        book[:isbn].each do |num|
+          expect(num).to be_a(String)
+        end
+
+        expect(book).to have_key(:title)
+        expect(book[:title]).to be_a(String)
+
+        expect(book).to have_key(:publisher)
+        expect(book[:publisher]).to be_a(Array)
+        book[:publisher].each do |pub|
+          expect(pub).to be_a(String)
+        end
+      end
+    end
   end
 end
