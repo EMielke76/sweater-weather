5d201b843ce4de1a132842225f46722367024c4f
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-03-07 14:05:17 -0700

    Feat: user#generate_api_key passing tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   validates :email, :presence => true, :uniqueness => true
-  validates :password_digest, :presence => true 
+  validates :password_digest, :presence => true
 
   has_secure_password
+
+  def generate_api_key
+    self.api_key = ApiKey.create.access_token
+  end
 end
