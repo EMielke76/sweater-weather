a9956a02fc28bcc5eb095412d68b661ca3901c7f
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-03-07 14:20:29 -0700

    User#create sad-path testing complete

--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -44,11 +44,53 @@ RSpec.describe "Registering a user" do
   context 'sad path' do
     it 'returns an error if email is a duplicate' do
       VCR.use_cassette('duplicate-email') do
+        user = User.create!(email: "faker@notreal.net", password: "yup", password_confirmation: "yup")
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+        params = {
+          "email"=>"faker@notreal.net",
+           "password"=>"123ThisIsFake",
+           "password_confirmation"=>"123ThisIsFake",
+          }
+
+        post "/api/v1/users", headers: headers, params: JSON.generate(params)
+        results = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(400)
+
+        expect(results).to have_key(:status)
+        expect(results[:status]).to eq(400)
+
+        expect(results).to have_key(:message)
+        expect(results[:message]).to eq("Email has already been taken")
+
+        expect(results).to have_key(:data)
+        expect(results[:data]).to eq({})
       end
     end
 
     it 'returns an error is passowords do not match' do
       VCR.use_cassette('passwords-do-not-match') do
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+        params = {
+          "email"=>"faker@notreal.net",
+           "password"=>"123ThisIsFake",
+           "password_confirmation"=>"123ThisIsReal",
+          }
+
+        post "/api/v1/users", headers: headers, params: JSON.generate(params)
+        results = JSON.parse(response.body, symbolize_names: true)
+        
+        expect(response).to have_http_status(400)
+
+        expect(results).to have_key(:status)
+        expect(results[:status]).to eq(400)
+
+        expect(results).to have_key(:message)
+        expect(results[:message]).to eq("Password confirmation doesn't match Password")
+
+        expect(results).to have_key(:data)
+        expect(results[:data]).to eq({})
       end
     end
   end
