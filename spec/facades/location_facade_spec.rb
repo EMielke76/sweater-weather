82f6f1548b0c11fe8e24c6bc81856c305930c607
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-03-08 14:01:57 -0700

    Create tests to handle impossible roadtrips

--- spec/facades/location_facade_spec.rb
+++ spec/facades/location_facade_spec.rb
@@ -14,15 +14,35 @@ RSpec.describe LocationFacade do
     end
 
     describe '#roadtrip' do
-      it 'returns Roadtrip data', :vcr do
-        start = "New York,NY"
-        finish = "Los Angeles,CA"
+      context 'successful trip' do
+        it 'returns Roadtrip data', :vcr do
+          start = "New York,NY"
+          finish = "Los Angeles,CA"
 
-        query = LocationFacade.new
-        results = query.roadtrip(start, finish)
+          query = LocationFacade.new
+          results = query.roadtrip(start, finish)
 
-        expect(results).to be_a(Roadtrip)
+          expect(results).to be_a(Roadtrip)
+        end
       end
+
+      context 'unsuccessful trip' do
+        it 'returns a hash if trip is impossible', :vcr do
+          start = "Los Angeles,CA"
+          finish = "London, UK"
+
+          query = LocationFacade.new
+          results = query.roadtrip(start, finish)
+
+          expect(results).to be_a(Hash)
+          expect(results).to have_key(:origin)
+          expect(results[:origin]).to eq(start)
+          expect(results).to have_key(:end)
+          expect(results[:end]).to eq(finish)
+          expect(results).to have_key(:message)
+          expect(results[:message]).to eq("Impossible Route")
+        end
+      end 
     end
   end
 end
--- spec/requests/api/v1/road_trips/create_spec.rb
+++ spec/requests/api/v1/road_trips/create_spec.rb
@@ -56,7 +56,55 @@ RSpec.describe 'Create a Road Trip endpoint' do
     end
 
     it 'returns data if route is deemed impossible' do
-      VCR.use_cassette('NY-to-London') do
+      VCR.use_cassette('LA-to-London') do
+        api_key = ApiKey.create
+        user = User.create!(email: "faker@notreal.net", password: "123ThisIsFake", password_confirmation: "123ThisIsFake", api_key: api_key.access_token )
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+        params = {
+             "origin"=>"Los Angeles,CA",
+             "destination"=>"London, UK",
+             "api_key" => "#{api_key.access_token}"
+          }
+
+        post '/api/v1/road_trip', headers: headers, params: JSON.generate(params)
+
+        results = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(201)
+
+        expect(results).to be_a(Hash)
+        expect(results).to have_key(:data)
+        expect(results[:data]).to be_a(Hash)
+
+        data = results[:data]
+        expect(data).to have_key(:id)
+        expect(data[:id]).to eq(nil)
+
+        expect(data).to have_key(:type)
+        expect(data[:type]).to eq("roadtrip")
+
+        expect(data).to have_key(:attributes)
+        expect(data[:attributes]).to be_a(Hash)
+
+        attributes = data[:attributes]
+        expect(attributes).to have_key(:start_city)
+        expect(attributes[:start_city]).to be_a(String)
+
+        expect(attributes).to have_key(:end_city)
+        expect(attributes[:end_city]).to be_a(String)
+
+        expect(attributes).to have_key(:travel_time)
+        expect(attributes[:travel_time]).to eq("Impossible Route")
+
+        expect(attributes).to have_key(:weather_at_eta)
+        expect(attributes[:weather_at_eta]).to be_a(Hash)
+
+        weather = attributes[:weather_at_eta]
+        expect(weather).to have_key(:temperature)
+        expect(weather[:temperature]).to eq("")
+
+        expect(weather).to have_key(:conditions)
+        expect(weather[:conditions]).to eq("")
       end
     end
   end
