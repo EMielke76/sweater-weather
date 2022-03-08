70b280bfb2b0c9d999ccfba8bb0bbc1617297f3a
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-03-08 10:25:21 -0700

    Feat: ForestService#roadtrip passing tests

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -5,6 +5,11 @@ class ForecastService
     body = parse_json(response)
   end
 
+  def roadtrip(lat, long)
+    response = conn.get("/data/2.5/onecall?lat=#{lat}&lon=#{long}&exclude=current,minutely,daily,alerts&units=imperial")
+    body = parse_json(response)
+  end 
+
   private
   def conn
     Faraday.new(url: "https://api.openweathermap.org") do |faraday|
