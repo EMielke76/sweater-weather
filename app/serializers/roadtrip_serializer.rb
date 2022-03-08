d89cf87935f3dced840ba49d8f48480988133307
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-03-08 11:05:48 -0700

    Create Roadtrip Serializer

---
+++ app/serializers/roadtrip_serializer.rb
@@ -0,0 +1,25 @@
+class RoadtripSerializer
+  class << self
+    def serialize(adventure, weather)
+      { data:
+        { id: nil,
+          type: 'roadtrip',
+          attributes: {
+                  start_city: adventure.start_city,
+                  end_city: adventure.end_city,
+                  travel_time: adventure.travel_time,
+                  weather_at_eta: {
+                    temperature: destination_forecast(adventure, weather).temperature,
+                    conditions: destination_forecast(adventure, weather).conditions
+                  }
+
+                      }
+        }
+      }
+    end
+
+    def destination_forecast(adventure, weather)
+      weather[adventure.travel_time.to_i]
+    end
+  end
+end
