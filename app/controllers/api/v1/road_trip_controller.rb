26b9a7f8c908e34d99244bcb9846a30200a6a71b
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-03-08 14:02:55 -0700

    Feat: RoadTrip#Create passing tests on local and Postman

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -3,7 +3,11 @@ class Api::V1::RoadTripController < ApplicationController
 
   def create
     adventure = LocationFacade.new.roadtrip(params[:origin], params[:destination])
-    weather = ForecastFacade.new.roadtrip(adventure.end_lat, adventure.end_long)
-    json_response(RoadtripSerializer.serialize(adventure, weather), :created)
+    if adventure.class == Hash
+      json_response(MissionImpossibleSerializer.serialize(adventure), :created)
+    else
+      weather = ForecastFacade.new.roadtrip(adventure.end_lat, adventure.end_long)
+      json_response(RoadtripSerializer.serialize(adventure, weather), :created)
+    end
   end
 end
