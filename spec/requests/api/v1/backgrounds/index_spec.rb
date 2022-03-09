9fe6e58888b5b4812a00e5873f019526702c4114
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-03-08 19:07:04 -0700

    Add tests to validate locations

--- spec/requests/api/v1/backgrounds/index_spec.rb
+++ spec/requests/api/v1/backgrounds/index_spec.rb
@@ -40,4 +40,26 @@ RSpec.describe 'Background image endpoint' do
       end
     end
   end
+
+  context 'sad path' do
+    it 'returns an error if location is empty' do
+      VCR.use_cassette('no-location') do
+        get "/api/v1/backgrounds?location="
+
+        results = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(400)
+
+        expect(results).to be_a(Hash)
+        expect(results).to have_key(:data)
+        expect(results[:data]).to eq({})
+
+        expect(results).to have_key(:status)
+        expect(results[:status]).to eq("BAD REQUEST")
+
+        expect(results).to have_key(:message)
+        expect(results[:message]).to eq("Location cannot be blank")
+      end
+    end
+  end
 end
--- spec/requests/api/v1/forecasts/index_spec.rb
+++ spec/requests/api/v1/forecasts/index_spec.rb
@@ -106,4 +106,26 @@ RSpec.describe 'Forecast Endpoint' do
       end
     end
   end
+
+  context 'sad path' do
+    it 'returns an error if location is blank' do
+      VCR.use_cassette('location-blank') do
+
+        get "/api/v1/forecast?location="
+
+        results = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(400)
+        expect(results).to have_key(:data)
+        expect(results[:data]).to be_a(Hash)
+        expect(results[:data]).to eq({})
+
+        expect(results).to have_key(:status)
+        expect(results[:status]).to eq("BAD REQUEST")
+
+        expect(results).to have_key(:message)
+        expect(results[:message]).to eq("Location cannot be blank")
+      end
+    end
+  end
 end
