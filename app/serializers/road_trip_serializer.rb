f8f289e97f90e08c977ea6caa92a4746b98f8b66
Eric Mielke <BeardBikesandBeers@gmail.com>
2022-03-08 22:06:04 -0700

    refactor out logic

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -9,17 +9,13 @@ class RoadTripSerializer
                   end_city: adventure.end_city,
                   travel_time: adventure.travel_time,
                   weather_at_eta: {
-                    temperature: destination_forecast(adventure, weather).temperature,
-                    conditions: destination_forecast(adventure, weather).conditions
+                    temperature: weather[adventure.travel_time.to_i].temperature,
+                    conditions: weather[adventure.travel_time.to_i].conditions
                   }
 
                       }
         }
       }
-    end
-
-    def destination_forecast(adventure, weather)
-      weather[adventure.travel_time.to_i]
     end
   end
 end
